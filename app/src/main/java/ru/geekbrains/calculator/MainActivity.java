58a53768fb0b634555fafd49e29501685bb8f32c
Nikolai <ndrovalev@gmail.com>
2022-02-02 12:18:50 +0300

    написал обработку каждой кнопки из макета калькулятора.

--- app/src/main/java/ru/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/MainActivity.java
@@ -3,12 +3,82 @@ package ru.geekbrains.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screen = findViewById(R.id.screen);
+    }
+    public void numbers(View view){
+        String number = screen.getText().toString();
+        switch (view.getId()){
+            case R.id.zero:
+                number += "0";
+                break;
+            case R.id.one:
+                number += "1";
+                break;
+            case R.id.two:
+                number += "2";
+                break;
+            case R.id.three:
+                number += "3";
+                break;
+            case R.id.four:
+                number += "4";
+                break;
+            case R.id.five:
+                number += "5";
+                break;
+            case R.id.six:
+                number += "6";
+                break;
+            case R.id.seven:
+                number += "7";
+                break;
+            case R.id.eight:
+                number += "8";
+                break;
+            case R.id.nine:
+                number += "9";
+                break;
+            case R.id.point:
+                number += ".";
+                break;
+            case R.id.delete_everything:
+                number = "";
+                break;
+            case R.id.delete_one_sign:
+                String newNumber = number.substring(0, number.length()-1);
+                number = newNumber;
+                break;
+            case R.id.percent:
+                number += "%";
+                break;
+            case R.id.multiply:
+                number += "*";
+                break;
+            case R.id.minus:
+                number += "-";
+                break;
+            case R.id.plus:
+                number += "+";
+                break;
+            case R.id.divide:
+                number += "÷";
+                break;
+            case R.id.equals:
+                number += "=";
+                break;
+        }
+        screen.setText(number);
     }
 }
