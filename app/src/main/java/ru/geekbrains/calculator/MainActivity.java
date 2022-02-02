304d1acd69b30333cba184e8f7b9be284050186b
Nikolai <ndrovalev@gmail.com>
2022-02-02 15:09:27 +0300

    Добавил методы сохранения и восстановления состояния Activity. при повороте экрана информация восстанавливается.

--- app/src/main/java/ru/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/MainActivity.java
@@ -1,15 +1,18 @@
 package ru.geekbrains.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView screen;
+    TextView screen;
+    String newScreen;
+    final static String screenKey = "key";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,65 +20,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         screen = findViewById(R.id.screen);
     }
-    public void numbers(View view){
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(screenKey, screen.getText().toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        screen.setText(savedInstanceState.getString(screenKey));
+    }
+
+    public void numbers(View view) {
         String number = screen.getText().toString();
-        switch (view.getId()){
-            case R.id.zero:
+        switch (view.getId()) {
+            case (R.id.zero):
                 number += "0";
                 break;
-            case R.id.one:
+            case (R.id.one):
                 number += "1";
                 break;
-            case R.id.two:
+            case (R.id.two):
                 number += "2";
                 break;
-            case R.id.three:
+            case (R.id.three):
                 number += "3";
                 break;
-            case R.id.four:
+            case (R.id.four):
                 number += "4";
                 break;
-            case R.id.five:
+            case (R.id.five):
                 number += "5";
                 break;
-            case R.id.six:
+            case (R.id.six):
                 number += "6";
                 break;
-            case R.id.seven:
+            case (R.id.seven):
                 number += "7";
                 break;
-            case R.id.eight:
+            case (R.id.eight):
                 number += "8";
                 break;
-            case R.id.nine:
+            case (R.id.nine):
                 number += "9";
                 break;
-            case R.id.point:
+            case (R.id.point):
                 number += ".";
                 break;
-            case R.id.delete_everything:
+            case (R.id.delete_everything):
                 number = "";
                 break;
-            case R.id.delete_one_sign:
-                String newNumber = number.substring(0, number.length()-1);
+            case (R.id.delete_one_sign):
+                String newNumber = number.substring(0, number.length() - 1);
                 number = newNumber;
                 break;
-            case R.id.percent:
+            case (R.id.percent):
                 number += "%";
                 break;
-            case R.id.multiply:
+            case (R.id.multiply):
                 number += "*";
                 break;
-            case R.id.minus:
+            case (R.id.minus):
                 number += "-";
                 break;
-            case R.id.plus:
+            case (R.id.plus):
                 number += "+";
                 break;
-            case R.id.divide:
+            case (R.id.divide):
                 number += "÷";
                 break;
-            case R.id.equals:
+            case (R.id.equals):
                 number += "=";
                 break;
         }
